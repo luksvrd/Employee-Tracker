1a6ad56792f62482158f4e614911c7de73f1dc8e
luksvrd <ljvy9b@gmail.com>
2023-01-11 23:12:43 -0600

    add config file

--- app/config.js
+++ app/config.js
@@ -1,1 +1,12 @@
+import dotenv from "dotenv";
 
+dotenv.config();
+
+export default {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+};
